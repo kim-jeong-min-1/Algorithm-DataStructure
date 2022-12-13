b12049441d861538a7a7d116a4df7d1fd7ce13b1
Jeongmin Kim <kjmgi0671@naver.com>
2022-12-13 18:47:27 +0900

    List

---
+++ Algorithm/MyList.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+public class MyList<T> 
+{
+    const int DEFAULT_SIZE = 1;
+    
+    T[] data = new T[DEFAULT_SIZE];
+
+    public int Count 
+    {
+        get { return data.Length; }
+    } // 현재 사용중인 배열의 개수
+
+    public int Capacity; // 예약된 배열의 개수
+
+   
+
+}
