6d31b9e7a610c69eb903d1ad7ef01c1afe03f9fc
Jeongmin Kim <kjmgi0671@naver.com>
2023-04-10 14:58:23 +0900

    Astar

---
+++ Algorigthm/Algorigthm/Astar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorigthm
+{
+    class Astar 
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int boardSize;
+            Board board = new Board();
+
+            do
+            {
+                boardSize = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                board.InitializeBoard(boardSize);
+                board.DrawBoard();
+            } while (boardSize % 2 == 0);
+
+
+        }
+    }
+}
